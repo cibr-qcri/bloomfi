e1cf6a0235123a3e152735780b45e80033ce111d
Yazan Boshmaf <yazan.boshmaf@gmail.com>
2022-06-09 10:32:53 +0300

    Issue #10: Refactoring

---
+++ services/analyze-sentiment/server.py
@@ -0,0 +1,19 @@
+'''A RESTful API server to analyze English tweets.'''
+
+from fastapi import FastAPI
+from analyzer import SentimentAnalyzer
+
+sentiment_analyzer = SentimentAnalyzer()
+app = FastAPI()
+
+
+@app.get('/')
+def get_root():
+    '''Get the root of the API.'''
+    return {"message": "Welcome to the sentiment analysis API!"}
+
+
+@app.get('/analyze')
+async def query_sentiment_analysis(text):
+    '''Get the sentiment of passed text.'''
+    return sentiment_analyzer.compute_scores(text)
