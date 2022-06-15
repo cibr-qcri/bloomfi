29c49b47480b4081f96179c29946c270ecc1bc6d
Yazan Boshmaf <yazan.boshmaf@gmail.com>
2022-06-15 10:22:21 +0300

    Issue #12: Adds a website template

--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,26 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import { StyledEngineProvider } from '@mui/material/styles';
+
 import reportWebVitals from './reportWebVitals';
+
+import App from './components/App';
+
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <StyledEngineProvider injectFirst>
+          <App />
+        </StyledEngineProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
